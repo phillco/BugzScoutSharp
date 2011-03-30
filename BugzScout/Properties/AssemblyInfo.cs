6ad7223410abe9833fca9fff1a9947d1c5797ae5
Phillip Cohen <pc@phillipcohen.net>
2011-03-30 17:28:23 -0500

    Added AssemblyInfo.cs version info. Setting version to 1.5.0.

--- BugzScout/Properties/AssemblyInfo.cs
+++ BugzScout/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "BugzScout" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Awesome error reporting with C# and FogBugz" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Microsoft" )]
-[assembly: AssemblyProduct( "BugzScout" )]
-[assembly: AssemblyCopyright( "Copyright © Microsoft 2011" )]
+[assembly: AssemblyCompany( "Phillip Cohen" )]
+[assembly: AssemblyProduct( "BugzScoutSharp" )]
+[assembly: AssemblyCopyright( "Copyright © Fog Creek Software 2011" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.5.0.0" )]
+[assembly: AssemblyFileVersion( "1.5.0.0" )]
