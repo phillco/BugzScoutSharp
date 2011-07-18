a60337bc47a3ac64e4d0ea066a23961351d11a7c
Phillip Cohen <pc@phillipcohen.net>
2011-07-18 13:35:30 -0600

    Improved the "missing-BugzScout.dll" handling. It now uses CurrentDomain_AssemblyResolve.

--- SampleProgram/Program.cs
+++ SampleProgram/Program.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using System.Threading;
 using SampleProgram.UI;
 using System.IO;
+using System.Reflection;
 
 namespace SampleProgram
 {
@@ -22,6 +23,7 @@ namespace SampleProgram
 
                 // Set up error reporting.
                 Application.SetUnhandledExceptionMode( UnhandledExceptionMode.CatchException );
+                AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler( CurrentDomain_AssemblyResolve );
                 AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler( CurrentDomain_UnhandledException );
                 Application.ThreadException += new ThreadExceptionEventHandler( Application_ThreadException );
 
@@ -29,16 +31,22 @@ namespace SampleProgram
             }
             catch ( Exception ex ) // Exceptions in Main() aren't caught by either Application_ThreadException or CurrentDomain_UnhandledException.
             {
-                try
-                {
-                    ErrorHandler.HandleUncaughtException( ex, true );
-                }
-                catch ( FileNotFoundException e )
-                {
-                    if ( e.Message.Contains( "BugzScout" ) ) // Occurs when the BugzScout assembly could not be loaded.
-                        MessageBox.Show( "There was an error loading some of this program's components.\nPlease download a new version.", "SameApp", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
-                }
+                ErrorHandler.HandleUncaughtException( ex, true );               
             }
+        }
+
+        /// <summary>
+        /// Occurs when the application references an assembly that's missing (IE, the user copied only the EXE file).
+        /// </summary>
+        static Assembly CurrentDomain_AssemblyResolve( object sender, ResolveEventArgs args )
+        {
+            if ( args.Name.StartsWith( "BugzScout" ) )
+            {
+                MessageBox.Show( "There was an error loading some of this program's components.\nPlease download a new version.", "SampleApp", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+                Environment.Exit( -2 );
+            }
+            
+            return null;
         }
 
         static void Application_ThreadException( object sender, ThreadExceptionEventArgs e )
